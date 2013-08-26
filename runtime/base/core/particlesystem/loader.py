8a7c761ba71a7b5195bc9ebbc2c5279029ffa23e
Harry Kalogirou <ckalogirou@customedialabs.com>
2013-08-26 16:42:08 +0300

    license change in python source files

--- runtime/base/core/particlesystem/loader.py
+++ runtime/base/core/particlesystem/loader.py
@@ -4,7 +4,20 @@ This file is part of the "Sylphis" 3D engine
 Copyright (c) 2002 - 2006 by Harry Kalogirou
 All rights reserved. Email: harkal@sylphis3d.com
 
-<<--LICENSE-->>
+	Copyright (c) 2013 Charilaos Kalogirou.
+	All rights reserved.
+
+	Redistribution and use in source and binary forms are permitted
+	provided that the above copyright notice and this paragraph are
+	duplicated in all such forms and that any documentation,
+	advertising materials, and other materials related to such
+	distribution and use acknowledge that the software was developed
+	by Charilaos Kalogirou. The name of the
+	Charilaos Kalogirou may not be used to endorse or promote products derived
+	from this software without specific prior written permission.
+	THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+	IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 
 """
 
