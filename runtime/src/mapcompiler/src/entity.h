71bdc1699ed4dcda4d99410a55351fb90c6f1c56
Harry Kalogirou <ckalogirou@customedialabs.com>
2013-08-26 16:33:50 +0300

    source file license text change

--- runtime/src/mapcompiler/src/entity.h
+++ runtime/src/mapcompiler/src/entity.h
@@ -7,34 +7,20 @@
   ***************************************************************************
     This file is part of "Sylphis3D Game Engine".
 
-    "Sylphis3D Game Engine" is free software; you can redistribute it and/or 
-    modify it under the terms of the GNU General Public License as 
-    published by the Free Software Foundation; either version 2 of the 
-    License, or (at your option) any later version, 
-    plus a special exception:
-      Linking "Sylphis3D Game Engine" statically or dynamically with other 
-    modules is making a combined work based on this library. Thus, the 
-    terms and conditions of the GNU General Public License cover the whole 
-    combination. As a special exception, the copyright holders of this 
-    library give you permission to link this library with independent 
-    modules to produce an executable, regardless of the license terms of 
-    these independent modules, and to copy and distribute the resulting 
-    executable under terms of your choice, provided that you also meet, 
-    for each linked independent module, the terms and conditions of the 
-    license of that module. An independent module is a module which is 
-    not derived from or based on this program. If you modify this program,
-    you may extend this exception to your version of the library, but you 
-    are not obligated to do so. If you do not wish to do so, delete this 
-    exception statement from your version.
+	Copyright (c) 2013 Charilaos Kalogirou.
+	All rights reserved.
 
-    "Sylphis3D Game Engine" is distributed in the hope that it will be 
-    useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with "Sylphis3D Game Engine"; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+	Redistribution and use in source and binary forms are permitted
+	provided that the above copyright notice and this paragraph are
+	duplicated in all such forms and that any documentation,
+	advertising materials, and other materials related to such
+	distribution and use acknowledge that the software was developed
+	by Charilaos Kalogirou. The name of the
+	Charilaos Kalogirou may not be used to endorse or promote products derived
+	from this software without specific prior written permission.
+	THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+	IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  ***************************************************************************/
 #ifndef _ENTITY_H_
 #define _ENTITY_H_
